e9357f56e4174eeee76d88992bb9bc5542faf5e7
youngier <1160415122@qq.com>
2020-10-15 20:32:26 +0800

    更新gradle版本、启用databinding、support包更换为androidx

--- app/src/main/java/com/young/listitem/MainActivity.java
+++ app/src/main/java/com/young/listitem/MainActivity.java
@@ -2,15 +2,16 @@ package com.young.listitem;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.young.listitem.style1.ListItemStyle1Activity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
--- app/src/main/java/com/young/listitem/style1/ListItemStyle1Activity.java
+++ app/src/main/java/com/young/listitem/style1/ListItemStyle1Activity.java
@@ -1,14 +1,14 @@
 package com.young.listitem.style1;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 import com.young.listitem.R;
 
 import java.util.ArrayList;
